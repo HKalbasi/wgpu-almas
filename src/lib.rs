d00c3a24f43094a93e2849159dfab87d7afc420d
hkalbasi <hamidrezakalbasi@protonmail.com>
2022-07-26 23:59:47 +0430

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::{f32::consts::PI, iter};
 
 use lazy_static::lazy_static;
-use rand::{Rng, prelude::ThreadRng};
+use rand::{prelude::ThreadRng, Rng};
 use wgpu::util::DeviceExt;
 use winit::{
     event::*,
@@ -56,7 +56,14 @@ lazy_static! {
         fn add_face(indices: &mut [Vec<u16>; 4], a: u16, b: u16, c: u16, tx: usize) {
             indices[tx % 4].extend([a, b, c]);
         }
-        fn add_random_face(indices: &mut [Vec<u16>; 4], vertices: &mut Vec<(f32, f32, f32)>, a: u16, b: u16, c: u16, rng: &mut ThreadRng) {
+        fn add_random_face(
+            indices: &mut [Vec<u16>; 4],
+            vertices: &mut Vec<(f32, f32, f32)>,
+            a: u16,
+            b: u16,
+            c: u16,
+            rng: &mut ThreadRng,
+        ) {
             let choice = rng.gen_range(0i32..10);
             match choice {
                 0..=1 => {
@@ -86,7 +93,7 @@ lazy_static! {
                         add_vertex(vertices, x * r + n * q, y * r + m * q, z * r + p * q)
                     };
                     add_face(indices, a, b, c, rng.gen());
-                    add_random_face(indices, vertices, d, e, a, rng);    
+                    add_random_face(indices, vertices, d, e, a, rng);
                 }
                 8..=9 => add_face(indices, a, b, c, rng.gen()),
                 _ => unreachable!(),
@@ -130,29 +137,6 @@ lazy_static! {
     static ref TX3_CNT: u64 = INDICES_TX3.len() as u64;
     static ref TX4_CNT: u64 = INDICES_TX4.len() as u64;
 }
-
-/*const VERTICES: &[Vertex] = &[
-    Vertex {
-        position: [-0.0868241, 0.49240386, 0.0],
-        tex_coords: [0.1, 0.1],
-    }, // A
-    Vertex {
-        position: [-0.49513406, 0.06958647, 0.0],
-        tex_coords: [0.9, 0.9],
-    }, // B
-    Vertex {
-        position: [-0.21918549, -0.44939706, 0.0],
-        tex_coords: [0.2, 0.7],
-    }, // C
-    Vertex {
-        position: [0.35966998, -0.3473291, 0.0],
-        tex_coords: [0.4, 0.2],
-    }, // D
-    Vertex {
-        position: [0.44147372, 0.2347359, 0.0],
-        tex_coords: [0.5, 0.3],
-    }, // E
-];*/
 
 #[rustfmt::skip]
 pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
@@ -247,10 +231,7 @@ impl CameraController {
                 }
                 true
             }
-            WindowEvent::MouseWheel {
-                delta,
-                ..
-            } => {
+            WindowEvent::MouseWheel { delta, .. } => {
                 match delta {
                     MouseScrollDelta::LineDelta(_, x) => self.zoom_offset += x,
                     _ => unreachable!(),
@@ -306,7 +287,8 @@ impl CameraController {
         let forward_mag = forward.magnitude();
 
         let right = forward_norm.cross(camera.up);
-        let rotate_vec = (self.prev_cursor.0 - self.current_cursor.0) as f32 * right - (self.prev_cursor.1 - self.current_cursor.1) as f32 * camera.up;
+        let rotate_vec = (self.prev_cursor.0 - self.current_cursor.0) as f32 * right
+            - (self.prev_cursor.1 - self.current_cursor.1) as f32 * camera.up;
         camera.eye = camera.target - (forward + rotate_vec * self.speed).normalize() * forward_mag;
         self.prev_cursor = self.current_cursor;
 
@@ -316,25 +298,18 @@ impl CameraController {
         camera.eye += forward_norm * self.zoom_offset;
         self.zoom_offset = 0.;
 
-        // Prevents glitching when camera gets too close to the
-        // center of the scene.
         if self.is_forward_pressed && forward_mag > self.speed {
-            camera.eye -= cgmath::vec3(0., 0.1, 0.);
+            camera.eye -= cgmath::vec3(0., 0.3, 0.);
         }
         if self.is_backward_pressed {
-            camera.eye += cgmath::vec3(0., 0.1, 0.);
+            camera.eye += cgmath::vec3(0., 0.3, 0.);
         }
 
         if self.is_right_pressed {
-            // Rescale the distance between the target and eye so
-            // that it doesn't change. The eye therefore still
-            // lies on the circle made by the target and eye.
-            //camera.eye = camera.target - (forward + right * self.speed).normalize() * forward_mag;
-            camera.eye -= cgmath::vec3(0.1, 0., 0.);
+            camera.eye -= cgmath::vec3(0.3, 0., 0.);
         }
         if self.is_left_pressed {
-            camera.eye += cgmath::vec3(0.1, 0., 0.);
-            //camera.eye = camera.target - (forward - right * self.speed).normalize() * forward_mag;
+            camera.eye += cgmath::vec3(0.3, 0., 0.);
         }
     }
 }
@@ -348,10 +323,7 @@ struct State {
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
     index_buffer: wgpu::Buffer,
-    #[allow(dead_code)]
-    diffuse_texture: texture::Texture,
     diffuse_bind_group: wgpu::BindGroup,
-    // NEW!
     camera: Camera,
     camera_controller: CameraController,
     camera_uniform: CameraUniform,
@@ -366,8 +338,6 @@ impl State {
     async fn new(window: &Window) -> Self {
         let size = window.inner_size();
 
-        // The instance is a handle to our GPU
-        // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU
         let instance = wgpu::Instance::new(wgpu::Backends::all());
         let surface = unsafe { instance.create_surface(window) };
         let adapter = instance
@@ -383,15 +353,14 @@ impl State {
                 &wgpu::DeviceDescriptor {
                     label: None,
                     features: wgpu::Features::empty(),
-                    // WebGL doesn't support all of wgpu's features, so if
-                    // we're building for the web we'll have to disable some.
+
                     limits: if cfg!(target_arch = "wasm32") {
                         wgpu::Limits::downlevel_webgl2_defaults()
                     } else {
                         wgpu::Limits::default()
                     },
                 },
-                None, // Trace path
+                None,
             )
             .await
             .unwrap();
@@ -646,12 +615,11 @@ impl State {
                 strip_index_format: None,
                 front_face: wgpu::FrontFace::Ccw,
                 cull_mode: Some(wgpu::Face::Back),
-                // Setting this to anything other than Fill requires Features::POLYGON_MODE_LINE
-                // or Features::POLYGON_MODE_POINT
+
                 polygon_mode: wgpu::PolygonMode::Fill,
-                // Requires Features::DEPTH_CLIP_CONTROL
+
                 unclipped_depth: false,
-                // Requires Features::CONSERVATIVE_RASTERIZATION
+
                 conservative: false,
             },
             depth_stencil: None,
@@ -660,8 +628,6 @@ impl State {
                 mask: !0,
                 alpha_to_coverage_enabled: false,
             },
-            // If the pipeline will be used with a multiview render pass, this
-            // indicates how many array layers the attachments will have.
             multiview: None,
         });
 
@@ -678,7 +644,11 @@ impl State {
                 .into_boxed_slice(),
         );
 
-        let indices = Box::leak([*INDICES_TX1, *INDICES_TX2, *INDICES_TX3, *INDICES_TX4].concat().into_boxed_slice());
+        let indices = Box::leak(
+            [*INDICES_TX1, *INDICES_TX2, *INDICES_TX3, *INDICES_TX4]
+                .concat()
+                .into_boxed_slice(),
+        );
 
         let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Vertex Buffer"),
@@ -700,7 +670,6 @@ impl State {
             render_pipeline,
             vertex_buffer,
             index_buffer,
-            diffuse_texture,
             diffuse_bind_group,
             diffuse_bind_group2,
             diffuse_bind_group3,
@@ -799,7 +768,11 @@ impl State {
             render_pass.set_index_buffer(
                 self.index_buffer.slice(
                     (INDICES_TX1.len() + INDICES_TX2.len() + INDICES_TX3.len()) as u64 * 2
-                        ..(INDICES_TX1.len() + INDICES_TX2.len() + INDICES_TX3.len() + INDICES_TX4.len()) as u64 * 2,
+                        ..(INDICES_TX1.len()
+                            + INDICES_TX2.len()
+                            + INDICES_TX3.len()
+                            + INDICES_TX4.len()) as u64
+                            * 2,
                 ),
                 wgpu::IndexFormat::Uint16,
             );
@@ -820,58 +793,52 @@ pub async fn run() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-    // State::new uses async code, so we're going to wait for it to finish
     let mut state = State::new(&window).await;
 
-    event_loop.run(move |event, _, control_flow| {
-        match event {
-            Event::WindowEvent {
-                ref event,
-                window_id,
-            } if window_id == window.id() => {
-                if !state.input(event) {
-                    match event {
-                        WindowEvent::CloseRequested
-                        | WindowEvent::KeyboardInput {
-                            input:
-                                KeyboardInput {
-                                    state: ElementState::Pressed,
-                                    virtual_keycode: Some(VirtualKeyCode::Escape),
-                                    ..
-                                },
-                            ..
-                        } => *control_flow = ControlFlow::Exit,
-                        WindowEvent::Resized(physical_size) => {
-                            state.resize(*physical_size);
-                        }
-                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                            // new_inner_size is &mut so w have to dereference it twice
-                            state.resize(**new_inner_size);
-                        }
-                        _ => {}
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => {
+            if !state.input(event) {
+                match event {
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            },
+                        ..
+                    } => *control_flow = ControlFlow::Exit,
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
                     }
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        state.resize(**new_inner_size);
+                    }
+                    _ => {}
                 }
             }
-            Event::RedrawRequested(window_id) if window_id == window.id() => {
-                state.update();
-                match state.render() {
-                    Ok(_) => {}
-                    // Reconfigure the surface if it's lost or outdated
-                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
-                        state.resize(state.size)
-                    }
-                    // The system is out of memory, we should probably quit
-                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-                    // We're ignoring timeouts
-                    Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
-                }
-            }
-            Event::MainEventsCleared => {
-                // RedrawRequested will only trigger once, unless we manually
-                // request it.
-                window.request_redraw();
-            }
-            _ => {}
         }
+        Event::RedrawRequested(window_id) if window_id == window.id() => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+
+                Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                    state.resize(state.size)
+                }
+
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+
+                Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
+            }
+        }
+        Event::MainEventsCleared => {
+            window.request_redraw();
+        }
+        _ => {}
     });
 }
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -14,6 +14,8 @@ struct VertexInput {
 struct VertexOutput {
     @builtin(position) clip_position: vec4<f32>,
     @location(0) tex_coords: vec2<f32>,
+    @location(1) world_normal: vec3<f32>,
+    @location(2) world_position: vec3<f32>,
 }
 
 @vertex
@@ -22,6 +24,10 @@ fn vs_main(
 ) -> VertexOutput {
     var out: VertexOutput;
     out.tex_coords = model.tex_coords;
+    out.clip_position = camera.view_proj * vec4<f32>(model.position, 1.0);
+    out.world_normal = model.position;
+    //var world_position: vec4<f32> = model_matrix * vec4<f32>(model.position, 1.0);
+    out.world_position = model.position;
     out.clip_position = camera.view_proj * vec4<f32>(model.position, 1.0);
     return out;
 }
@@ -35,5 +41,18 @@ var s_diffuse: sampler;
 
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    return textureSample(t_diffuse, s_diffuse, in.tex_coords);
+    let object_color: vec4<f32> = textureSample(t_diffuse, s_diffuse, in.tex_coords);
+    
+    let ambient_strength = 1.;
+    let ambient_color = vec3<f32>(1., 1., 1.) * ambient_strength;
+
+    let light_dir = normalize(vec3<f32>(1., 5., 1.) - in.world_position);
+                            //^^^^^^^^^^^^^^^^^^^^^ light position
+
+    let diffuse_strength = max(dot(in.world_normal, light_dir), 0.0);
+    let diffuse_color = vec3<f32>(1., 1., 1.) * diffuse_strength * 10.;
+
+    let result = (ambient_color + diffuse_color) * object_color.xyz;
+
+    return vec4<f32>(result, object_color.a);
 }

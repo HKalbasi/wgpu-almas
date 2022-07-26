54dc912cba689882c50e4c7feafad44690abcbec
hkalbasi <hamidrezakalbasi@protonmail.com>
2022-07-26 20:51:23 +0430

    init

---
+++ src/camera.rs
@@ -0,0 +1,18 @@
+pub struct Camera {
+    pub eye: cgmath::Point3<f32>,
+    pub target: cgmath::Point3<f32>,
+    pub up: cgmath::Vector3<f32>,
+    pub aspect: f32,
+    pub fovy: f32,
+    pub znear: f32,
+    pub zfar: f32,
+}
+
+impl Camera {
+    pub fn build_view_projection_matrix(&self) -> [[f32; 4]; 4] {
+        let view = cgmath::Matrix4::look_at_rh(self.eye, self.target, self.up);
+        let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
+        let r = proj * view;
+        r.into()
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use wgpu_almas::run;
+
+fn main() {
+    pollster::block_on(run());
+}
